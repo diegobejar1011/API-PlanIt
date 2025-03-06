9f3f421698be29581315011138643194148d3573
diegobejar2004 <diegobejar2004@gmail.com>
2025-03-05 19:40:03 -0600

    Correcion: Cambio en las credenciales de la base de datos

--- core/data/mysql/domain/config.ts
+++ core/data/mysql/domain/config.ts
@@ -1,8 +1,8 @@
 import { ConnectionOptions } from "mysql2";
 
 export const DataConfig: ConnectionOptions= {
-    host: "127.0.0.1",
-    user: "root",
-    password: "Nanami04?",
+    host: "db-planit.cjy0nlhvgoxa.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "planit2025",
     database: "PlanIt"
 }
